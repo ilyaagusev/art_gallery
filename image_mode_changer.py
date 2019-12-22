4f6d43d22ff91bff604014c8e8b8d90f4c6744ea
Ilya Gusev <gusev.ilya.a@gmail.com>
2019-12-22 14:52:49 +0300

    Deleted unnecessary files. Added README.md

--- image_mode_changer.py
+++
@@ -1,7 +0,0 @@
-
-
-def change_mode(image_name):
-    image = image_name
-    cmyk_image = image.convert('RGB')
-    cmyk_image.save('rgb_{}'.format(image_name))
-    print('mode changed to {}'.format(cmyk_image.mode))
--- overview.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-
-def rotate_image(image_name):
-    example_image = Image.open(image_name)
-    rotated_image = example_image.rotate(45)
-    rotated_image.save('rotated_{0}'.format(image_name))
