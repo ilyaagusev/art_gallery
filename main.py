ecf989440e7deed0e21120c766551a6e0565328f
Ilya Gusev <gusev.ilya.a@gmail.com>
2019-12-23 17:24:33 +0300

    prints, indexes removed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PIL import Image
 
 from blend_image import blend_image
 from crop_image import crop_image
-from image_info import image_information
 from merge_image_by_color import merge_rgb_image
 from split_image_by_color import split_image
 
@@ -15,21 +14,13 @@ def crop_and_blend(color_mode, left_crop, right_crop):
 
 
 image_name = Image.open('monro.jpg')
-splited_image_1 = split_image(image_name)
+red_channel, green_channel, blue_channel = split_image(image_name)
 
-red_color_mode = splited_image_1[0]
-green_color_mode = splited_image_1[1]
-blue_color_mode = splited_image_1[2]
-
-red_image_blend = crop_and_blend(red_color_mode, 100, 0)
-blue_image_blend = crop_and_blend(blue_color_mode, 0, 100)
-green_color_crop = crop_image(green_color_mode, 50, 0, 50, 0)
+red_image_blend = crop_and_blend(red_channel, 100, 0)
+blue_image_blend = crop_and_blend(green_channel, 0, 100)
+green_color_crop = crop_image(blue_channel, 50, 0, 50, 0)
 
 splited_image_2 = (red_image_blend, green_color_crop, blue_image_blend)
 merged_image = merge_rgb_image(splited_image_2)
 merged_image.thumbnail((80, 80))
 merged_image.save('monro_2.jpg')
-
-
-print(image_information('monro.jpg'))
-print(image_information('monro_2.jpg'))
